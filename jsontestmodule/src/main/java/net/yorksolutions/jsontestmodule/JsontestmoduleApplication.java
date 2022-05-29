0e2f83af5db7ec493405389c143520112fea0d30
Seethalakshmi <seethatkbhavan@gmail.com>
2022-05-28 21:48:04 -0500

    First jsontest commit with 2 modules

---
+++ jsontestmodule/src/main/java/net/yorksolutions/jsontestmodule/JsontestmoduleApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.jsontestmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsontestmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsontestmoduleApplication.class, args);
+    }
+
+}
---
+++ jsontestmodule/src/test/java/net/yorksolutions/jsontestmodule/JsontestmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.jsontestmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsontestmoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
